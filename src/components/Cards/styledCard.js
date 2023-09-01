1eb521870a705399ff78166a2e45e478e5d8eddc
nicolascalvo73 <nicolascalvo73@gmail.com>
2023-09-01 18:59:02 -0300

    card al 1-9

--- src/components/Cards/styledCard.js
+++ src/components/Cards/styledCard.js
@@ -1,30 +1,31 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const StyledCard = styled.div`
-  display: flex;
-  width: 21.4375rem;
-  height: 8.5rem;
-  padding: 0.5rem;
-  align-items: flex-start;
-  gap: 0.5rem;
-  border-radius: 1rem;
-  background: ${(props) => props.theme.primary};
-  box-shadow: ${(props) => props.theme.shortShadow};
+	display: flex;
+	width: 90%;
+	height: 8.5rem;
+	padding: 0.5rem;
+	align-items: flex-start;
+	gap: 0.5rem;
+	border-radius: 1rem;
+	background: ${(props) => props.theme.primary};
+	box-shadow: ${(props) => props.theme.shortShadow};
 
-  a {
-    text-decoration: none;
-  }
+	a {
+		text-decoration: none;
+	}
 
-  img {
-    height: 7.8rem;
-    flex-shrink: 0;
-    align-self: stretch;
-    object-fit: cover;
-    border-radius: 0.5rem;
-  }
+	img {
+		width: 7.8rem;
+		height: 7.5rem;
+		flex-shrink: 0;
+		align-self: stretch;
+		object-fit: cover;
+		border-radius: 0.5rem;
+	}
 
-  &:hover {
-    transform: scale(1.02);
-    transition: all 0.2s ease-in-out;
-  }
-`;
+	&:hover {
+		transform: scale(1.02);
+		transition: all 0.2s ease-in-out;
+	}
+`
