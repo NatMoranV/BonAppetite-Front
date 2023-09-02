55eb0bbd75d57a2d19d2bc607ffa7f69e5aa8f96
JuanUruloki <juampy_1@live.com>
2023-09-02 02:11:39 -0300

    FamilyCard completa y comenzando Carrosuel

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { StyledCard, InfoDiv, PriceDiv } from './styledCard'
-import { CircleButton } from '../CircleButton/CircleButton'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { StyledCard, InfoDiv, PriceDiv } from "./styledCard";
+import { CircleButton } from "../CircleButton/CircleButton";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export const Card = (props) => {
-	const { id, img, name, shortDesc, price, time } = props
-	return (
-		<StyledCard>
-			<img src={img} alt="image" />
-			<InfoDiv>
-				<h5>{name}</h5>
-				<p>{shortDesc}</p>
+  const { id, img, name, shortDesc, price, time } = props;
+  return (
+    <StyledCard>
+      <img src={img} alt="image" />
+      <InfoDiv>
+        <h5>{name}</h5>
+        <p>{shortDesc}</p>
 
-				<h7>{time} min</h7>
-				<PriceDiv>
-					<h6>${price}</h6>
-					<CircleButton className={'small'} icon={faPlus} />
-				</PriceDiv>
-			</InfoDiv>
-		</StyledCard>
-	)
-}
+        <>{time} min</>
+        <PriceDiv>
+          <h6>${price}</h6>
+          <CircleButton className={"small"} icon={faPlus} />
+        </PriceDiv>
+      </InfoDiv>
+    </StyledCard>
+  );
+};
--- src/components/Familys/StyledFamilyCard.js
+++ src/components/Familys/StyledFamilyCard.js
@@ -1,10 +1,36 @@
 import styled from "styled-components";
 
 export const StyledFamilyCard = styled.div`
-display: flex;
-width: 6 rem;
-padding: 0.5 rem;
-flex-direction: column;
-align-items: center;
-gap: 0.5 rem;
-`
+  scroll-snap-align: start;
+  display: flex;
+  width: 6rem;
+  height: 6.5rem;
+  padding: 0.5rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 0.5rem;
+  border-radius: 1rem;
+  background: ${(props) => props.theme.primary};
+  box-shadow: ${(props) => props.theme.shortShadow};
+
+  h6 {
+    text-align: center;
+    font-size: 16px;
+    position: relative;
+    margin-top: -0.5rem;
+
+    /* font-style: normal;
+    font-weight: 600;
+    line-height: normal; */
+  }
+
+  img {
+    height: 5rem;
+    align-self: stretch;
+    border-radius: 0.5rem;
+    flex-shrink: 0;
+    align-self: stretch;
+    object-fit: cover;
+  }
+`;
