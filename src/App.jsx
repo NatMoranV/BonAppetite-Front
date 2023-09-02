5a511edb07848fabf032efe36ffb63c40884c297
nicolascalvo73 <nicolascalvo73@gmail.com>
2023-09-01 23:46:14 -0300

    condicional navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import React, { useState } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import React, { useState } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
-import { NavBar } from "./components/Nav/NavBar";
-import { Footer } from "./components/Footer/Footer";
+import { NavBar } from './components/Nav/NavBar'
+import { Footer } from './components/Footer/Footer'
 
-import GlobalStyle from "./assets/GlobalStyles";
-import themes from "./assets/themes";
-import { Cart } from "./pages/CartPage/Cart";
-import { Home } from "./pages/HomePage/Home";
+import GlobalStyle from './assets/GlobalStyles'
+import themes from './assets/themes'
+import { Cart } from './pages/CartPage/Cart'
+import { Home } from './pages/HomePage/Home'
 
 function App() {
-  const [theme, setTheme] = useState("light");
+	const [theme, setTheme] = useState('light')
+	const { pathname } = useLocation()
+	const themeToggler = () => {
+		theme === 'dark' ? setTheme('light') : setTheme('dark')
+	}
 
-  const themeToggler = () => {
-    theme === "dark" ? setTheme("light") : setTheme("dark");
-  };
-
-  return (
-    <ThemeProvider theme={theme === "dark" ? themes.dark : themes.light}>
-      <GlobalStyle />
-      <div className="App">
-        <NavBar themeToggler={themeToggler} currentTheme={theme} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-        <Footer />
-      </div>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme === 'dark' ? themes.dark : themes.light}>
+			<GlobalStyle />
+			<div className="App">
+				{pathname !== '/' && <NavBar themeToggler={themeToggler} currentTheme={theme} />}
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/cart" element={<Cart />} />
+				</Routes>
+			</div>
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App
