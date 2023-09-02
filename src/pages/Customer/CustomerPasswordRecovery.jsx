32b77c826c98a2991e27cc517ed2ef54df037b86
nicolascalvo73 <nicolascalvo73@gmail.com>
2023-09-02 18:32:54 -0300

    maqueta de form de ancargados y recupero de pass

---
+++ src/pages/Customer/CustomerPasswordRecovery.jsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components'
+import { Logo } from '../../assets/images/Logo/Logo'
+import { StyledInput } from '../../components/Input/StyledInput'
+import { CallToAction } from '../../components/TextButton/CallToAction'
+
+export const CustomerPasswordRecovery = () => {
+	return (
+		<DivContainer>
+			<Container>
+				<Logo />
+				<h6>Actualiza tu contraseña</h6>
+				<InputContainer>
+					<p>ejemplo@mail.com</p>
+					<span>Nueva contraseña</span>
+					<StyledInput type={'password'} name={'password'} placeholder={'8 digitos'} />
+					<span>Repite tu contraseña</span>
+					<StyledInput type={'password'} name={'password'} placeholder={'vuelve a ingresarla'} />
+				</InputContainer>
+				<CallToAction text={'Actualizar Contraseña'} />
+			</Container>
+		</DivContainer>
+	)
+}
+
+const DivContainer = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+const Container = styled.div`
+	padding-top: 6rem;
+	width: 90%;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-evenly;
+	@media (min-width: 768px) {
+		width: 50%;
+	}
+`
+const InputContainer = styled.div`
+	width: 100%;
+	height: 17.25rem;
+	margin: 0 1rem;
+	display: flex;
+	flex-direction: column;
+	align-items: start;
+
+	gap: 0.5rem;
+	&& p {
+		align-self: center;
+		font-family: Montserrat;
+		font-style: normal;
+		font-weight: 400;
+		font-size: 1rem;
+	}
+	&& span {
+		font-family: Montserrat;
+		font-style: normal;
+		align-self: flex-start;
+		font-size: 1.25rem;
+		font-weight: 600;
+	}
+`
