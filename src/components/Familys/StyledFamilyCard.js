7c19600a50d766432ad78d9b40be178c82ae3662
JuanUruloki <juampy_1@live.com>
2023-09-02 00:27:13 -0300

    Carrousel FamilyCard creados

---
+++ src/components/Familys/StyledFamilyCard.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledFamilyCard = styled.div`
+display: flex;
+width: 6 rem;
+padding: 0.5 rem;
+flex-direction: column;
+align-items: center;
+gap: 0.5 rem;
+`
