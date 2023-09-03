9b71a9922e55286d22258ad36ddc8f0ab9a5af59
nicolascalvo73 <nicolascalvo73@gmail.com>
2023-09-03 01:52:07 -0300

    sin novedades

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,28 +1,26 @@
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import styled from "styled-components";
-import { MenuCarousel } from "../../components/Carrousel/CarouselMenu";
-import { FamiliesCarousel } from "../../components/Carrousel/Carrousel";
-import { StyledInput } from "../../components/Input/StyledInput";
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import styled from 'styled-components'
+import { MenuCarousel } from '../../components/Carrousel/CarouselMenu'
+import { FamiliesCarousel } from '../../components/Carrousel/Carrousel'
+import { StyledInput } from '../../components/Input/StyledInput'
 
 const Container = styled.div`
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  margin-top: 4rem;
-  align-items: center;
-`;
+	width: 100vw;
+	display: flex;
+	flex-direction: column;
+	padding: 1rem;
+	margin-top: 4rem;
+	align-items: center;
+`
+
+const search = console.log('search')
 
 export const Home = () => {
-  return (
-    <Container>
-      <FamiliesCarousel />
-      <StyledInput
-        placeholder={"Buscar"}
-        icono={faMagnifyingGlass}
-        onClick={"Search"}
-      />
-      <MenuCarousel></MenuCarousel>
-    </Container>
-  );
-};
+	return (
+		<Container>
+			<FamiliesCarousel />
+			<StyledInput placeholder={'Buscar'} icono={faMagnifyingGlass} onClick={search} />
+			<MenuCarousel />
+		</Container>
+	)
+}
