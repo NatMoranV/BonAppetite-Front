46bd35e1d44a38b36d634917441b8542a5aa7a8d
Natanael Morán <nat.moran.v@gmail.com>
2023-09-03 03:11:00 -0600

    ajustes menores, todo ok

--- src/pages/DetailPage/Detail.jsx
+++ src/pages/DetailPage/Detail.jsx
@@ -1,5 +1,4 @@
 import { styled } from "styled-components";
-import { TextButton } from "../../components/TextButton/TextButton";
 import { CTAsContainer } from "../../components/CTAs/CTAsContainer";
 
 const articleDetails = {
