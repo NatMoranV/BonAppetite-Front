1047d2536d485761b2853b3c19254855ddb22d38
nicolascalvo73 <nicolascalvo73@gmail.com>
2023-09-08 12:11:53 -0300

    Provider Store fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import store from './redux/store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>
-
-);
+	<Provider store={store}>
+		<React.StrictMode>
+			<Router>
+				<App />
+			</Router>
+		</React.StrictMode>
+	</Provider>
+)
