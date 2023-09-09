cb0033ac101da72d0087b2f0e7115aa21982f9b8
JuanUruloki <juampy_1@live.com>
2023-09-09 19:49:51 -0300

    added action types

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,1 +1,54 @@
+// / / / / / / /  FAMILIAS / / / / / / / / // 
+
 export const GET_MENU = 'GET_MENU'
+
+export const GET_FAMILIES = 'GET_FAMILIES'
+
+export const POST_FAMILY = 'POST_FAMILY'
+
+export const DELTE_FAMILY = 'DELTE_FAMILY'
+
+// / / / / / / /  PLATOS / / / / / / / / // 
+
+export const GET_DISH = 'GET_DISH'
+
+export const POST_DISH = 'POST_DISH'
+
+export const DELTE_DISH = 'DELTE_DISH'
+
+export const PUT_DISH = 'PUT_DISH'
+
+export const PUT_DELETED_DISH = 'PUT_DELETED_DISH'
+
+// / / / / / / /  FILTROS / / / / / / / / // 
+
+export const FILTER_BY_DISH_NAME = 'GET_DISH_BY_NAME'
+
+export const FILTER_BY_FAMILY_NAME = 'FILTER_BY_FAMILY_NAME'
+
+export const FILTER_BY_STOCK = 'FILTER_BY_STOCK'
+
+export const FILTER_BY_DISPONIBILITY = 'FILTER_BY_DISPONIBILITY'
+
+export const FILTER_BY_PRICE = 'FILTER_BY_PRICE'
+
+export const FILTER_BY_RATING = 'FILTER_BY_RATING'
+
+export const FILTER_BY_DELETED_DISH = 'FILTER_BY_DELETED_DISH'
+
+export const  FILTER_BY_ORDER_STATUS = 'FILTER_BY_ORDER_STATUS'
+
+export const FILTER_ORDER_BY_USER = 'FILTER_ORDER_BY_USER'
+
+export const FILTER_BY_PAYMENT_STATUS = 'FILTER_BY_PAYMENT_STATUS'
+
+// / / / / / / /  PEDIDO / / / / / / / / // 
+
+export const POST_ORDER = 'POST_ORDER'
+
+export const PUT_ORDER_PAYMENT = 'PUT_ORDER_PAYMENT'
+
+export const PUT_ORDER_STATUS = 'PUT_ORDER_STATUS'
+
+export const DELETE_ORDER = 'DELETE_ORDER'
+
