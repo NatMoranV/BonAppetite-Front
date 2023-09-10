c9cee051269847e5d5ec023a746578ad7c07d8dc
JuanUruloki <juampy_1@live.com>
2023-09-10 14:41:29 -0300

    add and update metods added

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,5 +1,14 @@
 import axios from "axios";
-import { GET_DISH, GET_FAMILIES, GET_MENU } from "../actions/types";
+import {
+  GET_DISH,
+  GET_FAMILIES,
+  GET_MENU,
+  POST_DISH,
+  POST_FAMILY,
+  POST_ORDER,
+  PUT_DISH,
+  PUT_ORDER_STATUS,
+} from "../actions/types";
 
 export const getMenu = () => {
   return async (dispatch) => {
@@ -54,3 +63,75 @@ export const getFamilies = () => {
     }
   };
 };
+
+export const addDish = (data) => {
+  const apiUrl = "https://resto-p4fa.onrender.com/product";
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(apiUrl, data);
+      const dish = response.data;
+      dispatch({ type: POST_DISH, payload: dish });
+      console.log("Respuesta de la API:", dish);
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+    }
+  };
+};
+
+export const addFamily = (data) => {
+  const apiUrl = "https://resto-p4fa.onrender.com/productclass";
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(apiUrl, data);
+      const family = response.data;
+      dispatch({ type: POST_FAMILY, payload: family });
+      console.log("Respuesta de la API:", family);
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+    }
+  };
+};
+
+export const addOrder = (data) => {
+  const apiUrl = "https://resto-p4fa.onrender.com/order";
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(apiUrl, data);
+      const order = response.data;
+      dispatch({ type: POST_ORDER, payload: order });
+      console.log("Respuesta de la API:", order);
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+    }
+  };
+};
+
+export const updateDish = (id, data) => {
+  const apiUrl = `https://resto-p4fa.onrender.com/product/${id}`;
+  return async (dispatch) => {
+    try {
+      const response = await axios.put(apiUrl, data);
+      const updatedDish = response.data;
+      dispatch({ type: PUT_DISH, payload: updatedDish });
+      console.log("Respuesta de la API:", updatedDish);
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+    }
+  };
+};
+
+export const updateOrderStatus = (id, data) => {
+  const apiUrl = `https://resto-p4fa.onrender.com/order/status/${id}`;
+  return async (dispatch) => {
+    try {
+      const response = await axios.put(apiUrl, data);
+      const updatedOrderStatus = response.data;
+      dispatch({ type: PUT_ORDER_STATUS, payload: updatedOrderStatus });
+      console.log("Respuesta de la API:", updatedOrderStatus);
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+    }
+  };
+};
+
+
