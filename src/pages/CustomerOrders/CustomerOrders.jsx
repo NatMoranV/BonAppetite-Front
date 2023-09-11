5d7b619e5914abcce8ceeac7c42f0d3730698a4e
nicolascalvo73 <nicolascalvo73@gmail.com>
2023-09-11 20:51:31 -0300

    customer orders

--- src/pages/CustomerOrders/CustomerOrders.jsx
+++ src/pages/CustomerOrders/CustomerOrders.jsx
@@ -1,26 +1,27 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const CustomerOrders = () => {
-  return (
-    <StyledView>
-      <h1>CUSTOMER ORDERS</h1>
-    </StyledView>
-  );
-};
+	return (
+		<StyledView>
+			<h3>procesamos tu pago!</h3>
+			<p>cuando este listo te avisaremos.</p>
+		</StyledView>
+	)
+}
 
 const StyledView = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  margin: auto;
-  overflow-y: auto;
-  padding: 3vh 4vw 10vh;
-  box-sizing: border-box;
-  transition: width 0.3s ease-in-out;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	margin: auto;
+	overflow-y: auto;
+	padding: 3vh 4vw 10vh;
+	box-sizing: border-box;
+	transition: width 0.3s ease-in-out;
 
-  @media (min-width: 650px) {
-    width: 30rem;
-    padding: 15vh 0;
-  }
-`;
+	@media (min-width: 650px) {
+		width: 30rem;
+		padding: 15vh 0;
+	}
+`
