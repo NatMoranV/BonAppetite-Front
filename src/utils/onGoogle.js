955ea68022cc270313846dd17a4c9814cf005649
cesarbeloqui <120270198+cesarbeloqui@users.noreply.github.com>
2023-09-12 15:46:30 -0300

    register terminado

--- src/utils/onGoogle.js
+++ src/utils/onGoogle.js
@@ -2,14 +2,13 @@ import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import axios from "axios";
 import auth from "./config";
 
-const onGoogle = async () => {
+const onGoogle = async (navigate) => {
   const providerGoogle = new GoogleAuthProvider();
   try {
     const result = await signInWithPopup(auth, providerGoogle);
     const user = result.user;
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
-
     if (token) {
       try {
         const url = `${import.meta.env.VITE_URL_BACK}/register/`;
@@ -18,9 +17,14 @@ const onGoogle = async () => {
             "Content-Type": "application/json",
           },
         });
-        console.log({ ...user, token });
+        if (user.emailVerified) {
+          localStorage.setItem("accessToken", JSON.stringify(user.accessToken));
+          alert("Has iniciado sesión con Google");
+          navigate();
+        }
         return { ...user, token };
       } catch (error) {
+        alert("Error al registrarse recargue e intente de nuevo");
         return {
           message:
             "Error en registrar el usuario en Base Datos, intente nuevamente",
@@ -29,6 +33,7 @@ const onGoogle = async () => {
       }
     }
   } catch (error) {
+    alert("Error al iniciar sesión con Google");
     return { message: "Error al iniciar sesión con Google:", error };
   }
 };
--- src/utils/sigIn.js
+++ src/utils/sigIn.js
@@ -1,17 +1,36 @@
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-const sigIn = (email, password) => {
+const sigIn = (email, password, navigate) => {
   const auth = getAuth();
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-      console.log(user);
-      // ...
+      if (user.emailVerified) {
+        localStorage.setItem("accessToken", JSON.stringify(user.accessToken));
+        navigate();
+      } else {
+        alert("Por favor verifica tu email");
+      }
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      console.log(errorCode, errorMessage);
+      if (errorCode === "auth/wrong-password") {
+        alert("Contraseña incorrecta");
+      } else if (errorCode === "auth/user-not-found") {
+        alert("Usuario no encontrado");
+      } else if (errorCode === "auth/invalid-email") {
+        alert("Email invalido");
+      } else if (errorCode === "auth/user-disabled") {
+        alert("Usuario deshabilitado");
+      } else if (errorCode === "auth/too-many-requests") {
+        alert(
+          "Demasiados intentos el usuario fue inhabilitado por seguridad, deberá reestablecer su contraseña"
+        );
+      }
+
+      console.log(errorCode);
+      console.log(errorMessage);
     });
 };
 
