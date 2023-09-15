687b236a3e42b83a458120afe3ac33ec61845a5b
Natanael Morán <nat.moran.v@gmail.com>
2023-09-15 00:17:31 -0600

    pointer off logo review

--- src/assets/images/Logo/Logo.jsx
+++ src/assets/images/Logo/Logo.jsx
@@ -2,11 +2,33 @@ import { faPepperHot } from '@fortawesome/free-solid-svg-icons'
 
 import { styled } from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useLocation } from 'react-router-dom'
+
+
+
+
+export const Logo = ({onClick}) => {
+	const location = useLocation().pathname;
+    const isReview = location === "/review"
+	return (
+		<StyledLogo className="Logo" onClick={onClick} $isReview={isReview}>
+			<span>
+				<FontAwesomeIcon icon={faPepperHot} />
+			</span>
+			<h6>Bon Appétit</h6>
+		</StyledLogo>
+	)
+}
 
 const StyledLogo = styled.div`
 	display: flex;
 	gap: 1rem;
 	cursor: pointer;
+	${(props) => props.$isReview &&`
+	
+	cursor: default;
+	
+	`}
 
 	& span {
 		line-height: 2rem;
@@ -19,14 +41,3 @@ const StyledLogo = styled.div`
 	}
 
 `
-
-export const Logo = ({onClick}) => {
-	return (
-		<StyledLogo className="Logo" onClick={onClick}>
-			<span>
-				<FontAwesomeIcon icon={faPepperHot} />
-			</span>
-			<h6>Bon Appétit</h6>
-		</StyledLogo>
-	)
-}
