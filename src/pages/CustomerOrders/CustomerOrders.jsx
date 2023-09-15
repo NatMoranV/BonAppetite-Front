c515d9247a69807e1a26f739964d83de33bee94b
JuanUruloki <juampy_1@live.com>
2023-09-14 22:56:19 -0300

    Modal added to orders view

--- src/pages/CustomerOrders/CustomerOrders.jsx
+++ src/pages/CustomerOrders/CustomerOrders.jsx
@@ -1,37 +1,63 @@
-import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
-import { CTAsContainer } from '../../components/CTAs/CTAsContainer'
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+import { CTAsContainer } from "../../components/CTAs/CTAsContainer";
+import { Modal } from "../../components/Modal/Modal";
+import { useEffect, useState } from "react";
 
 export const CustomerOrders = () => {
-	const navigate = useNavigate()
-	const navigateHome = () => {
-		navigate('/customer')
-	}
-	return (
-		<StyledView>
-			<h6>Procesamos tu pago!</h6>
-			<p>Cuando este listo te avisaremos.</p>
-			<CTAsContainer text1={'Volver'} onClick1={navigateHome} />
-		</StyledView>
-	)
-}
+  const [loading, setLoading] = useState(false);
+  const { referrer } = useParams();
+  const navigate = useNavigate();
+  const navigateHome = () => {
+    setLoading(false);
+    navigate("/customer");
+  };
+
+  useEffect(() => {
+    console.log("referrer", referrer);
+    if (referrer === 'http://localhost:5173/customer/basket') {
+    console.log('Condición verdadera');
+    setLoading(true);
+	const timer = setTimeout(()=>{
+		setLoading(false)
+	},3000)
+	return () => clearTimeout(timer)
+  }
+  }, [referrer]);
+
+  return (
+    <StyledView>
+      {loading && (
+        <Modal
+          loading={loading}
+          title={"Procesamos tu pago!"}
+          msg={"Cuando este listo te avisaremos."}
+        />
+      )}
+      {/* <h6>Procesamos tu pago!</h6>
+			<p>Cuando este listo te avisaremos.</p> */}
+
+      <CTAsContainer text1={"Volver"} onClick1={navigateHome} />
+    </StyledView>
+  );
+};
 
 const StyledView = styled.div`
-	position: relative;
-	top: 12rem;
-	display: flex;
-	gap: 2rem;
-	flex-direction: column;
-	align-items: center;
-	width: 100%;
-	margin: auto;
-	overflow-y: auto;
-	padding: 3vh 4vw 10vh;
-	box-sizing: border-box;
-	transition: width 0.3s ease-in-out;
+  position: relative;
+  top: 12rem;
+  display: flex;
+  gap: 2rem;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin: auto;
+  overflow-y: auto;
+  padding: 3vh 4vw 10vh;
+  box-sizing: border-box;
+  transition: width 0.3s ease-in-out;
 
-	@media (min-width: 650px) {
-		width: 30rem;
-		padding: 15vh 0;
-	}
-`
+  @media (min-width: 650px) {
+    width: 30rem;
+    padding: 15vh 0;
+  }
+`;
