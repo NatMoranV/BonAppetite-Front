f55c105511d42775c7e36043a18bbd42f792f754
JuanfraReales <juanfranciscoreales@gmail.com>
2023-09-24 20:51:34 -0300

    Fix condicionamiento de navigate

--- src/pages/ExclusiveManager/EditFamilies.jsx
+++ src/pages/ExclusiveManager/EditFamilies.jsx
@@ -1,83 +1,84 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import { useEffect, useState } from 'react'
-import { Modal } from '../../components/Modal/Modal'
-import { TextButton } from '../../components/TextButton/TextButton'
+import { useEffect, useState } from "react";
+import { Modal } from "../../components/Modal/Modal";
+import { TextButton } from "../../components/TextButton/TextButton";
 
-import { CTAsContainer } from '../../components/CTAs/CTAsContainer'
-import { FamilyComponent } from '../../components/FamilyComponent/FamilyComponent'
-import { useDispatch, useSelector } from 'react-redux'
-import { deleteFamily, updateFamilies } from '../../redux/actions/actions'
-import { useNavigate } from 'react-router-dom'
+import { CTAsContainer } from "../../components/CTAs/CTAsContainer";
+import { FamilyComponent } from "../../components/FamilyComponent/FamilyComponent";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteFamily, updateFamilies } from "../../redux/actions/actions";
+import { useNavigate } from "react-router-dom";
 
 export const EditFamilies = () => {
-	const dispatch = useDispatch()
-	const allFamilies = useSelector((state) => state.families)
-	const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false)
-	const [itemToDeleteIndex, setItemToDeleteIndex] = useState(null)
-	const [families, setFamilies] = useState(allFamilies || [])
-	const navigate = useNavigate()
-	const userRole = useSelector((state) => state.userLogged)
+	const dispatch = useDispatch();
+	const allFamilies = useSelector((state) => state.families);
+	const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+	const [itemToDeleteIndex, setItemToDeleteIndex] = useState(null);
+	const [families, setFamilies] = useState(allFamilies || []);
+	const navigate = useNavigate();
+	const userRole = useSelector((state) => state.userLogged);
+
 	useEffect(() => {
-		if (userRole.role !== 'Manager' || userRole.role !== 'Admin') {
-			navigate('/')
+		if (userRole.role !== "Manager" && userRole.role !== "Admin") {
+			navigate("/");
 		}
-	}, [navigate])
+	}, [navigate]);
 
 	useEffect(() => {
 		if (allFamilies) {
-			setFamilies(allFamilies)
+			setFamilies(allFamilies);
 		}
-	}, [allFamilies])
+	}, [allFamilies]);
 
 	const handleAddFamily = () => {
 		const newFamily = {
-			class: '',
-			image: '',
-		}
+			class: "",
+			image: "",
+		};
 
-		const updatedFamilies = [...families, newFamily]
-		setFamilies(updatedFamilies)
-	}
+		const updatedFamilies = [...families, newFamily];
+		setFamilies(updatedFamilies);
+	};
 
 	const handleDelete = (index) => {
-		setIsDeleteModalVisible(true)
-		setItemToDeleteIndex(index)
-	}
+		setIsDeleteModalVisible(true);
+		setItemToDeleteIndex(index);
+	};
 
 	const handleConfirmDelete = () => {
 		if (itemToDeleteIndex !== null) {
-			const familyToDelete = families[itemToDeleteIndex]
-			const familyIdToDelete = familyToDelete.id
+			const familyToDelete = families[itemToDeleteIndex];
+			const familyIdToDelete = familyToDelete.id;
 
-			dispatch(deleteFamily(familyIdToDelete))
+			dispatch(deleteFamily(familyIdToDelete));
 
-			const updatedFamilies = [...families]
-			updatedFamilies.splice(itemToDeleteIndex, 1)
-			setFamilies(updatedFamilies)
+			const updatedFamilies = [...families];
+			updatedFamilies.splice(itemToDeleteIndex, 1);
+			setFamilies(updatedFamilies);
 
-			setIsDeleteModalVisible(false)
-			setItemToDeleteIndex(null)
+			setIsDeleteModalVisible(false);
+			setItemToDeleteIndex(null);
 		}
-	}
+	};
 
 	const handleCancelDelete = () => {
-		setIsDeleteModalVisible(false)
-		setItemToDeleteIndex(null)
-	}
+		setIsDeleteModalVisible(false);
+		setItemToDeleteIndex(null);
+	};
 
 	const handleUpdateFamilyDetails = (index, updatedDetails) => {
-		const updatedFamilies = [...families]
+		const updatedFamilies = [...families];
 		updatedFamilies[index] = {
 			...updatedFamilies[index],
 			...updatedDetails,
-		}
+		};
 
-		setFamilies(updatedFamilies)
-	}
+		setFamilies(updatedFamilies);
+	};
 
 	if (!allFamilies) {
-		return <Modal isLoader title={'Cargando...'} />
+		return <Modal isLoader title={"Cargando..."} />;
 	}
 
 	const post = async () => {
@@ -87,23 +88,25 @@ export const EditFamilies = () => {
 				class: family.class,
 				image: family.image,
 				enable: family.enable,
-			}))
+			}));
 
-			const postData = { updatedArray }
+			const postData = { updatedArray };
 
-			await dispatch(updateFamilies(postData))
-			console.log('Familias agregadas con éxito')
-			console.log(postData)
+			await dispatch(updateFamilies(postData));
+			console.log("Familias agregadas con éxito");
+			console.log(postData);
 		} catch (error) {
-			console.error('Error al agregar familias:', error)
+			console.error("Error al agregar familias:", error);
 		}
-	}
+	};
 
 	return (
 		<StyledView>
 			<Header>
 				<Title>Familias</Title>
-				<Subtitle>Ingresa las familias en el orden que deseas mostrarlos</Subtitle>
+				<Subtitle>
+					Ingresa las familias en el orden que deseas mostrarlos
+				</Subtitle>
 			</Header>
 			{families.map((family, index) => (
 				<FamilyComponent
@@ -114,28 +117,28 @@ export const EditFamilies = () => {
 					onUpdateDetails={handleUpdateFamilyDetails}
 				/>
 			))}
-			<TextButton text={'Agregar nueva familia'} onClick={handleAddFamily} />
+			<TextButton text={"Agregar nueva familia"} onClick={handleAddFamily} />
 			<CTAsContainer
 				onClick1={() => {
-					post()
+					post();
 				}}
-				text1={'Guardar cambios'}
+				text1={"Guardar cambios"}
 			/>
 
 			{isDeleteModalVisible && (
 				<Modal
 					onClose={handleCancelDelete}
-					title={'Borrar familia'}
-					msg={'Ten en cuenta que no podrás recuperarla.'}
-					text1={'Borrar'}
+					title={"Borrar familia"}
+					msg={"Ten en cuenta que no podrás recuperarla."}
+					text1={"Borrar"}
 					onClick1={handleConfirmDelete}
-					text2={'Cancelar'}
+					text2={"Cancelar"}
 					onClick2={handleCancelDelete}
 				/>
 			)}
 		</StyledView>
-	)
-}
+	);
+};
 
 const StyledView = styled.div`
 	display: flex;
@@ -153,21 +156,21 @@ const StyledView = styled.div`
 		width: 30rem;
 		padding: 15vh 1rem;
 	}
-`
+`;
 
 const Header = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	gap: 1rem;
-`
+`;
 
 const Title = styled.h6`
 	margin: 0;
-`
+`;
 
 const Subtitle = styled.span`
 	font-size: 1rem;
 	font-weight: 500;
 	margin: 0;
-`
+`;
