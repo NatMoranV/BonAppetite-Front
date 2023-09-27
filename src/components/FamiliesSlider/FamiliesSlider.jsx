bfacd7ceb3222bf43dcebb990d28b649a302ff61
nicolascalvo73 <nicolascalvo73@gmail.com>
2023-09-27 00:01:24 -0300

    filtros al cien?

--- src/components/FamiliesSlider/FamiliesSlider.jsx
+++ src/components/FamiliesSlider/FamiliesSlider.jsx
@@ -1,76 +1,77 @@
-import styled from "styled-components";
+/* eslint-disable react/prop-types */
+import styled from 'styled-components'
 
-import { FamilyCard } from "../Cards/FamilyCard";
-import { useDispatch, useSelector } from "react-redux";
-import { filterByFamily, getMenu } from "../../redux/actions/actions";
-import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { FamilyCard } from '../Cards/FamilyCard'
+import { useDispatch, useSelector } from 'react-redux'
+import { filterByFamily } from '../../redux/actions/actions'
+import { useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
 export const FamiliesSlider = ({ onClick }) => {
-  const location = useLocation().pathname;
+	const location = useLocation().pathname
 
-  const isCustomer = location === "/customer/";
+	const isCustomer = location === '/customer/'
 
-  const dispatch = useDispatch();
-  const allFamilies = useSelector((state) => state.families);
+	const dispatch = useDispatch()
+	const allFamilies = useSelector((state) => state.families)
 
-  const [activeFamily, setActiveFamily] = useState(null);
+	const [activeFamily, setActiveFamily] = useState(null)
 
-  const toggleFilterFamily = (family) => {
-    if (activeFamily === family) {
-      dispatch(getMenu());
-      setActiveFamily(null);
-    } else {
-      dispatch(filterByFamily(family));
-      setActiveFamily(family);
-    }
-  };
+	const toggleFilterFamily = (family) => {
+		if (activeFamily === family) {
+			dispatch(filterByFamily('none'))
+			setActiveFamily(null)
+		} else {
+			dispatch(filterByFamily(family))
+			setActiveFamily(family)
+		}
+	}
 
-  return (
-    <SliderContainer onClick={onClick}>
-      {allFamilies.map((card, index) => {
-        const hasProducts = card.Products.length > 0;
+	return (
+		<SliderContainer onClick={onClick}>
+			{allFamilies.map((card, index) => {
+				const hasProducts = card.Products.length > 0
 
-        if (!isCustomer || (isCustomer && card.enable && hasProducts)) {
-          return (
-            <FamilyCard
-              onClick={() => toggleFilterFamily(card.class)}
-              key={index}
-              name={card.class}
-              image={card.image}
-              id={card.id}
-              enable={hasProducts && card.enable}
-              hasProducts={hasProducts}
-              isActive={activeFamily === card.class}
-            />
-          );
-        }
-      })}
-    </SliderContainer>
-  );
-};
+				if (!isCustomer || (isCustomer && card.enable && hasProducts)) {
+					return (
+						<FamilyCard
+							onClick={() => toggleFilterFamily(card.class)}
+							key={index}
+							name={card.class}
+							image={card.image}
+							id={card.id}
+							enable={hasProducts && card.enable}
+							hasProducts={hasProducts}
+							isActive={activeFamily === card.class}
+						/>
+					)
+				}
+			})}
+		</SliderContainer>
+	)
+}
 
 const SliderContainer = styled.div`
-  display: flex;
-  gap: 1.5rem;
-  width: 100%;
-  justify-content: center;
-  height: auto;
-  box-sizing: border-box;
-  overflow-x: auto;
-  padding: 1rem;
-  transition: all 0.2s ease-in-out;
-  background-color: ${(props) => props.theme.primary};
-  z-index: 3;
+	display: flex;
+	gap: 1.5rem;
+	width: 100%;
+	justify-content: center;
+	height: auto;
+	box-sizing: border-box;
+	overflow-x: auto;
+	padding: 1rem;
+	transition: all 0.2s ease-in-out;
+	background-color: ${(props) => props.theme.primary};
+	z-index: 3;
 
-  @media (max-width: 650px) {
-    justify-content: left;
-  }
+	@media (max-width: 650px) {
+		justify-content: left;
+	}
 
-  &&::-webkit-scrollbar-thumb {
-    background: transparent;
-  }
-  &&::-webkit-scrollbar {
-    width: 0.01px;
-  }
-`;
+	&&::-webkit-scrollbar-thumb {
+		background: transparent;
+	}
+	&&::-webkit-scrollbar {
+		width: 0.01px;
+	}
+`
