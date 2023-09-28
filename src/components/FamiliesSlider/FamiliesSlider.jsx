7015f3d847b31cc1740c318f99f3cbccec82563e
Natanael Morán <nat.moran.v@gmail.com>
2023-09-27 22:21:29 -0600

    families ok

--- src/components/FamiliesSlider/FamiliesSlider.jsx
+++ src/components/FamiliesSlider/FamiliesSlider.jsx
@@ -1,77 +1,93 @@
 /* eslint-disable react/prop-types */
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import { FamilyCard } from '../Cards/FamilyCard'
-import { useDispatch, useSelector } from 'react-redux'
-import { filterByFamily } from '../../redux/actions/actions'
-import { useLocation } from 'react-router-dom'
-import { useState } from 'react'
+import { FamilyCard } from "../Cards/FamilyCard";
+import { useDispatch, useSelector } from "react-redux";
+import { filterByFamily } from "../../redux/actions/actions";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
-export const FamiliesSlider = () => {
-	const location = useLocation().pathname
+export const FamiliesSlider = ({ onClick }) => {
+  const location = useLocation().pathname;
 
-	const isCustomer = location === '/customer/'
+  const isCustomer = location === "/customer/";
 
-	const dispatch = useDispatch()
-	const allFamilies = useSelector((state) => state.families)
+  const dispatch = useDispatch();
+  const allFamilies = useSelector((state) => state.families);
 
-	const [activeFamily, setActiveFamily] = useState(null)
+  const familiesSelected = useSelector((state) => state.familiesToFilter);
 
-	const toggleFilterFamily = (family) => {
-		if (activeFamily === family) {
-			dispatch(filterByFamily(`${family}`))
-			setActiveFamily(null)
-		} else {
-			dispatch(filterByFamily(family))
-			setActiveFamily(family)
-		}
-	}
+  console.log(familiesSelected);
+  const [activeFamily, setActiveFamily] = useState(null);
 
-	return (
-		<SliderContainer>
-			{allFamilies.map((card, index) => {
-				const hasProducts = card.Products.length > 0 && card.Products.some((product) => product.enable)
+  const toggleFilterFamily = (family) => {
+    if (activeFamily === family) {
+      dispatch(filterByFamily(`${family}`));
+      setActiveFamily(null);
+    } else {
+      dispatch(filterByFamily(family));
+      setActiveFamily(family);
+    }
+  };
 
-				if (!isCustomer || (isCustomer && card.enable && hasProducts)) {
-					return (
-						<FamilyCard
-							onClick={() => toggleFilterFamily(card.class)}
-							key={index}
-							name={card.class}
-							image={card.image}
-							id={card.id}
-							enable={hasProducts && card.enable}
-							hasProducts={hasProducts}
-							isActive={activeFamily === card.class}
-						/>
-					)
-				}
-			})}
-		</SliderContainer>
-	)
-}
+  return (
+    <SliderContainer onClick={onClick}>
+      <FamiliesContainer>
+	  {allFamilies.map((card, index) => {
+        const hasProducts =
+          card.Products.length > 0 &&
+          card.Products.some((product) => product.enable);
+        const isActive = familiesSelected.includes(card.class);
+
+        if (!isCustomer || (isCustomer && card.enable && hasProducts)) {
+          return (
+            <FamilyCard
+              onClick={() => toggleFilterFamily(card.class)}
+              key={index}
+              name={card.class}
+              image={card.image}
+              id={card.id}
+              enable={hasProducts && card.enable}
+              hasProducts={hasProducts}
+              isActive={isActive}
+            />
+          );
+        }
+      })}
+	  </FamiliesContainer>
+    </SliderContainer>
+  );
+};
 
 const SliderContainer = styled.div`
-	display: flex;
-	gap: 1.5rem;
-	width: 100%;
-	justify-content: center;
-	height: auto;
-	box-sizing: border-box;
-	overflow-x: auto;
-	padding: 1rem;
-	transition: all 0.2s ease-in-out;
-	background-color: ${(props) => props.theme.primary};
-	z-index: 3;
+  display: flex;
+  gap: 1.5rem;
+  width: 100%;
+  height: auto;
+  margin: auto;
+  box-sizing: border-box;
+  overflow-x: auto;
+  padding: 1rem;
+  transition: all 0.2s ease-in-out;
+  background-color: ${(props) => props.theme.primary};
+  z-index: 3;
 
-	@media (max-width: 650px) {
-		justify-content: left;
-	}
+  @media (max-width: 650px) {
+    justify-content: left;
+  }
 
-	&&::-webkit-scrollbar-thumb {
-		background: transparent;
-	}
-	&&::-webkit-scrollbar {
-		width: 0.01px;
-	}
+  &&::-webkit-scrollbar-thumb {
+    background: transparent;
+  }
+  &&::-webkit-scrollbar {
+    width: 0.01px;
+  }
+`;
+
+
+const FamiliesContainer = styled.div`
+display: flex;
+gap: 1.5rem;
+width: auto;
+margin: auto;
 `
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,86 +1,98 @@
 /* eslint-disable react/prop-types */
-import { faPepperHot, faTimes, faCircleCheck } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import styled, { css, keyframes } from 'styled-components'
-import { CTAsContainer } from '../CTAs/CTAsContainer'
+import {
+  faPepperHot,
+  faTimes,
+  faCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled, { css, keyframes } from "styled-components";
+import { CTAsContainer } from "../CTAs/CTAsContainer";
 
-export const Modal = ({ isLoader, title, msg, text1, onClick1, text2, onClick2, onClose, iconOK }) => {
-	return (
-		<StyledModal>
-			<Overlay onClick={!isLoader ? onClose : null} />
+export const Modal = ({
+  isLoader,
+  title,
+  msg,
+  text1,
+  onClick1,
+  text2,
+  onClick2,
+  onClose,
+  iconOK,
+}) => {
+  return (
+    <StyledModal>
+      <Overlay onClick={!isLoader ? onClose : null} />
 
-			<ModalContainer>
-				{!isLoader && <CloseIcon icon={faTimes} onClick={onClose} />}
-				{isLoader ? (
-					<>
-						<StyledFontAwesomeIcon icon={faPepperHot} $isLoading={isLoader} />
+      <ModalContainer>
+        {!isLoader && <CloseIcon icon={faTimes} onClick={onClose} />}
+        {isLoader ? (
+          <>
+            <StyledFontAwesomeIcon icon={faPepperHot} $isLoading={isLoader} />
 
-						<StyledTitle>{title}</StyledTitle>
-					</>
-				) : (
-					<>
-						{!iconOK ? (
-							<StyledFontAwesomeIcon icon={faPepperHot} />
-						) : (
-							<StyledFontAwesomeIcon
-								icon={faCircleCheck}
-								style={{
-									'--fa-primary-color': '#329632',
-									'--fa-secondary-color': '#6cbc5c',
-								}}
-							/>
-						)}
-						<StyledTitle>{title}</StyledTitle>
-						<StyledMessage> {msg} </StyledMessage>
-						{text1 && <CTAsContainer text1={text1} onClick1={onClick1} text2={text2} onClick2={onClick2} />}
-					</>
-				)}
-			</ModalContainer>
-		</StyledModal>
-	)
-}
+            <StyledTitle>{title}</StyledTitle>
+          </>
+        ) : (
+          <>
+            <StyledFontAwesomeIcon icon={faPepperHot} />
+
+            <StyledTitle>{title}</StyledTitle>
+            <StyledMessage> {msg} </StyledMessage>
+            {text1 && (
+              <CTAsContainer
+                text1={text1}
+                onClick1={onClick1}
+                text2={text2}
+                onClick2={onClick2}
+              />
+            )}
+          </>
+        )}
+      </ModalContainer>
+    </StyledModal>
+  );
+};
 
 const StyledModal = styled.div`
-	position: fixed;
-	top: 0;
-	right: 0;
-	z-index: 4; 
-	width: 100%;
-	height: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-`
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Overlay = styled.div`
-	width: 100%;
-	height: 100%;
-	background: rgba(0, 0, 0, 0.8);
-	box-sizing: border-box;
-	position: absolute;
-`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  box-sizing: border-box;
+  position: absolute;
+`;
 
 const ModalContainer = styled.div`
-	position: relative;
-	height: auto;
-	width: 80%;
-	display: flex;
-	flex-direction: column;
-	gap: 2rem;
-	justify-content: space-evenly;
-	align-items: center;
-	box-sizing: border-box;
-	padding: 2rem 1rem;
-	border-radius: 1rem;
-	background: ${(props) => props.theme.primary};
-	@media (min-width: 650px) {
-		width: 30rem;
-	}
+  position: relative;
+  height: auto;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  justify-content: space-evenly;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 2rem 1rem;
+  border-radius: 1rem;
+  background: ${(props) => props.theme.primary};
+  @media (min-width: 650px) {
+    width: 30rem;
+  }
 
-	&& > div {
-		box-shadow: none;
-	}
-`
+  && > div {
+    box-shadow: none;
+  }
+`;
 
 const beat = keyframes`
   0% {
@@ -92,34 +104,34 @@ const beat = keyframes`
   100% {
     transform: scale(1);
   }
-`
+`;
 
 const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
-	font-size: 4rem;
+  font-size: 4rem;
 
-	${(props) =>
-		props.$isLoading &&
-		css`
-			animation: ${beat} 2s infinite ease-in-out;
-		`}
-`
+  ${(props) =>
+    props.$isLoading &&
+    css`
+      animation: ${beat} 2s infinite ease-in-out;
+    `}
+`;
 
 const StyledTitle = styled.span`
   text-align: center;
-	font-size: 2rem;
-	font-weight: 600;
-	margin-top: 0;
-`
+  font-size: 2rem;
+  font-weight: 600;
+  margin-top: 0;
+`;
 
 const StyledMessage = styled.span`
-	text-align: center;
-	font-size: 1.2rem;
-`
+  text-align: center;
+  font-size: 1.2rem;
+`;
 
 const CloseIcon = styled(FontAwesomeIcon)`
-	position: absolute;
-	top: 1rem;
-	right: 1rem;
-	font-size: 1.2rem;
-	cursor: pointer;
-`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  font-size: 2rem;
+  cursor: pointer;
+`;
