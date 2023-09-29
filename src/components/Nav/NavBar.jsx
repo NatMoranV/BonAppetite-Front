cf7fdcb6032f8d6d8274429dccc6c80fddd62d85
JuanUruloki <juampy_1@live.com>
2023-09-28 22:29:41 -0300

    conditioning userRole Navigation on NavBar

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -65,6 +65,17 @@ export const NavBar = ({ themeToggler, currentTheme }) => {
   }, 5000);
 
   useEffect(() => {
+
+		if (
+			userRole.role !== 'Manager' &&
+			location === '/manager/' &&
+			userRole.role !== 'Admin' &&
+			location === '/manager/'
+		) {
+			navigate('/')
+		
+	}
+
     function handleResize() {
       if (window.innerWidth >= 650) {
         setIsMenuOpen(false);
@@ -73,7 +84,7 @@ export const NavBar = ({ themeToggler, currentTheme }) => {
 
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [navigate,location,userRole]);
 
   const [confirmationPassword, setConfirmationPassword] = useState(false);
   const [confirmationLogout, setConfirmationLogout] = useState(false);
